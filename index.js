f471936a57058b8a9fa28585051e7fbd8e5104de
Developer-SantaCole <jorge.fiorilo@santacole.com>
2024-10-24 09:25:19 +0200

    creamos api para recibir parametros de colores de la franklin 3

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import mqtt from 'async-mqtt';
 import { config } from 'dotenv';
 import path from 'path';
+import os from 'os'; // Importar el módulo os
 
 // Cargar las variables de entorno
 config({
@@ -12,46 +13,75 @@ config({
 // Inicializar la aplicación Express
 const app = express();
 
+// Middleware para analizar JSON en el cuerpo de las solicitudes
+app.use(express.json());
+
 // Definir el puerto para el servidor
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+// Función para obtener la IP local de la máquina
+// function getLocalIp() {
+//     const interfaces = os.networkInterfaces();
+//     for (const interfaceName in interfaces) {
+//         const addresses = interfaces[ interfaceName ];
+//         for (const addr of addresses) {
+//             if (addr.family === 'IPv4' && !addr.internal) {
+//                 return addr.address; // Retornar la IP de la interfaz activa
+//             }
+//         }
+//     }
+//     return 'localhost'; // Fallback a localhost si no se encuentra otra IP
+// }
 
 // Ruta principal
 app.get('/prueba', async (req, res) => {
-    console.log(req);
-
-    console.log("valor de la variable"+ process.env.MQTT_BROKER_URL);
-
-    // Responder con un mensaje
-    res.json({ mensaje: "mundo" });
-
-    // Configuración para la conexión MQTT
-    const mqttBrokerUrl = process.env.MQTT_BROKER_URL;
-    const username = process.env.MQTT_USERNAME;
-    const password = process.env.MQTT_PASSWOR;
-    const deviceId = process.env.MQTT_DEVICE_ID;
-    const topic = `${deviceId}`;
-
-    var options = {
-        connectTimeout: 5 * 1000,
-        reconnectPeriod: 0,
-        clientId: "akenza-example",
-        username: username,
-        password: password,
-    };
-
-    const body = {
-        "state": "ON",
-        "color": {
-            "r": 255,
-            "g": 0,
-            "b": 0
-        },
-        "effect": "none",
-        "brightness": 255
-    };
-
     try {
-        // Conectar al broker MQTT
+        res.json({ mensaje: "hola mundo" });
+    } catch (e) {
+        console.error(`Error: ${e.stack}`);
+        res.status(500).json({ error: 'Error al publicar en MQTT' });
+    }
+});
+
+
+app.post('/franklin', async (req, res) => {
+    try {
+        const { red, green, blue } = req.body;
+
+        console.log(`Red: ${red}, Green: ${green}, Blue: ${blue}`);
+
+        console.log("-------------------------------");
+        console.log(req.body);
+        console.log("++++++++++++++++++++++++++++++++");
+        console.log(req);
+
+        // Configuración para la conexión MQTT
+        const mqttBrokerUrl = process.env.MQTT_BROKER_URL;
+        const username = process.env.MQTT_USERNAME;
+        const password = process.env.MQTT_PASSWOR;
+        const deviceId = process.env.MQTT_DEVICE_ID;
+        const topic = `${deviceId}`;
+
+        var options = {
+            connectTimeout: 5 * 1000,
+            reconnectPeriod: 0,
+            clientId: "akenza-example",
+            username: username,
+            password: password,
+        };
+
+        const body = {
+            "state": "ON",
+            "color": {
+                "r": red,
+                "g": green,
+                "b": blue
+            },
+            "effect": "none",
+            "brightness": 255
+        };
+
+        // Conectar al broker MQTT y publicar el mensaje
         const client = await mqtt.connectAsync(
             `tls://${mqttBrokerUrl}:8883`,
             options,
@@ -69,13 +99,20 @@ app.get('/prueba', async (req, res) => {
 
         // Cerrar la conexión
         await client.end();
-    } catch (e) {
+
+        // Responder al cliente con éxito
+        res.json({ message: 'Datos recibidos y publicados en MQTT correctamente' });
+
+
+    } catch (error) {
         console.error(`Error: ${e.stack}`);
-        res.status(500).json({ error: 'Error al publicar en MQTT' });
+        res.status(500).json({ error: 'Error al procesar la solicitud' });
     }
-});
+})
 
 // Iniciar el servidor
 app.listen(PORT, () => {
+    // const host = getLocalIp();
+    // console.log(host);
     console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
 });
