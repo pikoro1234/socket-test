fde731150cafcdffa88f2d974d4e50c04b31dd24
Developer-SantaCole <jorge.fiorilo@santacole.com>
2025-04-11 13:03:02 +0200

    actualizamos conexion a base de datos

--- database/bd_urbicomm.js
+++ database/bd_urbicomm.js
@@ -5,9 +5,9 @@ import path from 'path';
 config({ path: process.env.CONFIG_PATH || path.resolve(process.cwd(), '.env'), });
 
 export const pool_urbicomm = mysql.createPool({
-    host: process.env.HOST_DB_URBICOMM,
-    user: process.env.USER_DB_URBICOMM,
-    password: process.env.PASSWORD_DB_URBICOMM,
+    host: process.env.HOST_DB,
+    user: process.env.USER_DB,
+    password: process.env.PASSWORD_DB,
     database: process.env.DATABASE_DB_URBIDERMIS,
     waitForConnections: true,
     connectionLimit: 10,
@@ -15,9 +15,9 @@ export const pool_urbicomm = mysql.createPool({
 });
 
 export const pool_urbidata = mysql.createPool({
-    host: process.env.HOST_DB_URBICOMM,
-    user: process.env.USER_DB_URBICOMM,
-    password: process.env.PASSWORD_DB_URBICOMM,
+    host: process.env.HOST_DB,
+    user: process.env.USER_DB,
+    password: process.env.PASSWORD_DB,
     database: process.env.DATABASE_DB_URBIDATA,
     waitForConnections: true,
     connectionLimit: 10,
